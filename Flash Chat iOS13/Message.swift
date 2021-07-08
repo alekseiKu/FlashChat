1ff09cd98246b87c7d774041c6349b753c9ce2de
12121 <kulichkin8@mail.ru>
2021-07-08 20:13:04 +0300

    first commit

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by 001011011 on 18.02.2021.
+//
+
+import Foundation
+
+
+struct Message {
+    let sender: String
+    let body: String
+}
